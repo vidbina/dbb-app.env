e8b4e4ddb9bf164a76fe9abb63c807b7c444cada
David Asabina <vid@bina.me>
2017-11-21 16:25:01 +0100

    cleanup

--- default.nix
+++ default.nix
@@ -6,13 +6,6 @@ rec {
     src = ./.;
     version = "0.1.0";
 
-    #libudev_LIBS="${libudev.out}/lib";
-    #libudev_LIBS="${libudev.lib}";
-
-    #libudev_SRC="${libudev.out}/src";
-
-    udev = libudev;
-
     buildInputs = [
       autoconf
       automake
