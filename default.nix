780e53df6466629655e194e23813f2c1c23b009f
David Asabina <vid@bina.me>
2018-01-06 13:54:50 +0100

    helping Qt5 find some tools

--- default.nix
+++ default.nix
@@ -25,6 +25,25 @@ rec {
       libusb
       libqrencode
       pkgconfig
+
+      which
+    ];
+
+    QTDIR="${qt5.qtbase.dev}";
+    MOC="${qt5.qtbase.dev}/bin/moc";
+    UIC="${qt5.qtbase.dev}/bin/uic";
+    RCC="${qt5.qtbase.dev}/bin/rcc";
+    LRELEASE="${qt5.qttools.dev}/bin/lrelease";
+    LUPDATE="${qt5.qttools.dev}/bin/lupdate";
+
+    QTBASE="${qt5.qtbase.bin}";
+    QTBASE_DEV="${qt5.qtbase.dev}";
+    QTTOOLS="${qt5.qttools.bin}";
+    QTTOOLS_DEV="${qt5.qttools.dev}";
+
+    configureFlags = [
+      "--enable-debug"
+      "--enable-libusb"
     ];
 
     hardeningDisable = [
